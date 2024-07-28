70c47b8f9877d243379734d9f4caf4345cd5d0f9
Asfandyar Jalil <asfandyarjalil@gmail.com>
2024-07-28 14:09:48 +0500

    selectionsort and insertion sort algo

---
+++ DSA/insertionSort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+let arr = [6, 5, 3, 4, 7, 8];
+
+let resp = insertionSort(arr);
+console.log(resp);
---
+++ DSA/selectionSort.js
@@ -0,0 +1,39 @@
+// function selectionSort(arr) {
+//     let smallest=0;
+//     for (let i = 0; i < arr.length; i++) {
+//         let temp = arr[i];
+//         for (let j = 0; j < arr.length; j++) {
+//             if (arr[j] < arr[j + 1]) {
+//                 smallest = j;
+//                 // let temp = arr[j];
+//                 // arr[j] = arr[j + 1];
+//                 // arr[j + 1] = temp;
+//             } else {
+//                 smallest = j + 1;
+//             }
+//         }
+
+//         arr[i] = arr[smallest];
+//         arr[smallest] = temp;
+//     }
+//     return arr;
+// }
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        let temp = arr[i];
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
+let arr = [6, 5, 3, 4, 7, 8];
+
+let resp = selectionSort(arr);
+console.log(resp);
